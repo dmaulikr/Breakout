080cfea1e3ce2e30af32945a3f57f4478d3cfe57
Keita Tsutsui <keitanxkeitan@gmail.com>
2014-06-21 11:14:28 +0900

    First commit

---
+++ Breakout/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Breakout
+//
+//  Created by 筒井 啓太 on 2014/06/19.
+//  Copyright keitanxkeitan 2014年. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
